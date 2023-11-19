730c635ef770756d2885ed93f33ac39f59e3ad38
jjcourtney <jcourtneywork@gmail.com>
2023-11-19 21:28:41 +0000

    adds logic for storing state of the scores

--- sushi-go/app/page.tsx
+++ sushi-go/app/page.tsx
@@ -2,36 +2,72 @@
 
 import { useState } from "react";
 
-function PlayerRow({ number, name }: { number: number; name: string }) {
+type PlayerRowProps = {
+  number: number;
+  name: string;
+  setPlayerScore: any;
+  playerScore: any[];
+};
+
+function PlayerRow({
+  number,
+  name,
+  setPlayerScore,
+  playerScore,
+}: PlayerRowProps) {
+  const updatePlayerScore = (e: any) => {
+    const value = e.target.value;
+    const id = e.target.id.split("-");
+
+    const playerNumberInt = parseInt(id[1]) - 1;
+    const roundInt = id[3] === "bonus" ? 3 : parseInt(id[3]) - 1;
+
+    const newPlayerScore = [...playerScore];
+
+    newPlayerScore[playerNumberInt][roundInt] = parseInt(value);
+    setPlayerScore(newPlayerScore);
+  };
+
   return (
     <div className="flex flex-col items-center">
-      <input
-        className="md:w-48 w-28 h-12 rounded-lg border-2 border-gray-300 px-4 text-zinc-900"
-        placeholder={name}
-      />
+      <div className="md:w-48 w-28 h-12 rounded-lg border-2 order-gray-300 px-4 text-white">
+        <p className="font-bold text-center pt-2">{name}</p>
+      </div>
       <input
         className="md:w-48 w-28 h-12 rounded-lg border-2 border-gray-300 px-4  text-zinc-900"
         placeholder="0"
         type="number"
         id={`player-${number}-round-1`}
+        onChange={(e) => {
+          updatePlayerScore(e);
+        }}
       />
       <input
         className="md:w-48 w-28 h-12 rounded-lg border-2 border-gray-300 px-4  text-zinc-900"
         placeholder="0"
         type="number"
         id={`player-${number}-round-2`}
+        onChange={(e) => {
+          updatePlayerScore(e);
+        }}
       />
       <input
         className="md:w-48 w-28 h-12 rounded-lg border-2 border-gray-300 px-4  text-zinc-900"
         placeholder="0"
         type="number"
         id={`player-${number}-round-3`}
+        onChange={(e) => {
+          updatePlayerScore(e);
+        }}
       />
       <input
         className="md:w-48 w-28 h-12 rounded-lg border-2 border-gray-300 px-4  text-zinc-900"
         placeholder="0"
         type="number"
-        id={`player-${number}-bonus`}
+        id={`player-${number}-round-bonus`}
+        onChange={(e) => {
+          updatePlayerScore(e);
+        }}
       />
     </div>
   );
@@ -49,7 +85,7 @@ function RowTitle() {
     return (
       <div
         key={index}
-        className="md:w-48 w-28 h-12 rounded-lg border-2 border-gray-300 px-4"
+        className="pt-2 font-bold text-center md:w-48 w-28 h-12 rounded-lg border-2 border-gray-300 px-4"
       >
         {title}
       </div>
@@ -67,13 +103,27 @@ export default function Home() {
     if (players.includes(newPlayer)) return;
 
     setPlayers([...players, newPlayer]);
+    setPlayerScore([...playerScore, [0, 0, 0, 0]]);
   };
+
+  const [playerScore, setPlayerScore] = useState(
+    players.map(() => [0, 0, 0, 0])
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
       <div className="flex flex-row items-center">
         <RowTitle />
         {players.map((player, index) => {
-          return <PlayerRow name={player} number={index + 1} key={player} />;
+          return (
+            <PlayerRow
+              name={player}
+              number={index + 1}
+              key={player}
+              playerScore={playerScore}
+              setPlayerScore={setPlayerScore}
+            />
+          );
         })}
       </div>
       <div className="mt-10">
